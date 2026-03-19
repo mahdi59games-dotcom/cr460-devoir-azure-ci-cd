97e2a2e0c8d5ec5ba4aeacad282ddc09869455a2
Mohamed Al Mahdi <mahdi59games@gmail.com>
2026-03-18 23:28:56 -0400

    4eme debug variable explicites

--- provider.tf
+++ provider.tf
@@ -9,5 +9,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-  use_cli = false
+
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  use_cli         = false
 }
