7ee7e22e981051189bca2f09f13f0a7d5b114071
Mohamed Al Mahdi <mahdi59games@gmail.com>
2026-03-19 00:19:58 -0400

    ajout dun docker  question 10

--- main.tf
+++ main.tf
@@ -3,54 +3,28 @@ resource "azurerm_resource_group" "rg" {
   location = "canada central"
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-cr460-mahdi"
-  address_space       = ["10.0.0.0/16"]
+resource "azurerm_container_group" "aci" {
+  name                = "aci-cr460-mahdi"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-}
+  ip_address_type     = "Public"
+  dns_name_label      = "acimahdicr460"
+  os_type             = "Linux"
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-cr460-mahdi"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
+  container {
+    name   = "nginx"
+    image  = "nginx:latest"
+    cpu    = "0.5"
+    memory = "1.5"
 
-resource "azurerm_network_interface" "nic" {
-  name                = "nic-cr460-mahdi"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "vm-cr460-mahdi"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_B16as_v2"
-  admin_username      = "azureuser"
-  network_interface_ids = [
-    azurerm_network_interface.nic.id
-  ]
-
-  admin_password                  = var.vm_password
-  disable_password_authentication = false
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
   }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
+  exposed_port {
+    port     = 80
+    protocol = "TCP"
   }
 }
